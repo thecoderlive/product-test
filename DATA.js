f26021777ff86d6d6618ac0b5a379b1762c8feee
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-19 03:36:14 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,27 @@
+export const item = {
+    "id": "72bff659-b6a7-4125-b3ca-bf2dbf130dc5",
+    "gallery": [
+        {
+            "id": "c41e3743-86b0-45d4-9646-62685bcc8698",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+        },
+        {
+            "id": "06f60fa7-475f-4af2-a941-54cb2118f545",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/315.png"
+        },
+        {
+            "id": "25e6ccd5-57fb-4b87-9106-30c694e64bb1",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/158.png"
+        },
+        {
+            "id": "6049778c-2b3f-4add-987a-f50e7ff46156",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+        },
+        {
+            "id": "0f075d73-aef3-4b45-a0bc-6130190595c4",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/166.png"
+        }
+    ],
+    "event_name": "trade",
+    "event_description": "Successful suddenly than page thus personal capital. Individual upon see event. Indeed mouth recently operation tree service."
+}
